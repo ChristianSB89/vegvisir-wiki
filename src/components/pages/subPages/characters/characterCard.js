d57e4749c79f8c84f155de9c43a624bac96b9ece
ChristianSB <christiansb.kodehode@gmail.com>
2022-10-19 14:20:53 +0200

    Tested characterData array

--- src/components/pages/subPages/characters/characterCard.js
+++ src/components/pages/subPages/characters/characterCard.js
@@ -1,16 +1,34 @@
 import React from "react";
 import ProfilePic from "../../../img/profile-placeholder.png";
-import { CardWrapper, ProfileImg } from "./characterSmallCard";
+import { characterDataShort } from "./characterData";
+import {
+  CardWrapper,
+  ProfileImg,
+  RaceOriginFlex,
+  TxtDeco,
+} from "./characterSmallCard";
 
 function CharacterCard() {
   return (
     <CardWrapper>
-      <h3>Name</h3>
-      <ProfileImg src={ProfilePic} />
-      <h4>About</h4>
-      <p>Race:</p>
-      <p>Origins:</p>
-      <p>Description:</p>
+      {characterDataShort.map(
+        ({ id, image, name, race, origin, description }) => {
+          return (
+            <article key={id}>
+              <h3>{name}</h3>
+              <ProfileImg src={image} />
+              <RaceOriginFlex>
+                <TxtDeco>Race:</TxtDeco>
+                <p>{race}</p>
+                <TxtDeco>Origins:</TxtDeco>
+                <p>{origin}</p>
+              </RaceOriginFlex>
+              <TxtDeco>Description:</TxtDeco>
+              <p>{description}</p>
+            </article>
+          );
+        }
+      )}
     </CardWrapper>
   );
 }
---
+++ src/components/pages/subPages/characters/characterData.js
@@ -0,0 +1,33 @@
+import ProfilePic from "../../../img/profile-placeholder.png";
+
+//-------------------//
+// Short description //
+//-------------------//
+
+export const characterDataShort = [
+  {
+    id: 1,
+    image: ProfilePic,
+    name: "Gudleif Skrukkefyllhaugen",
+    race: "Human",
+    origin: "Klingsdalen",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.",
+  },
+];
+
+//--------------------//
+// Longer description //
+//--------------------//
+
+export const characterDataShort = [
+  {
+    id: 1,
+    image: ProfilePic,
+    name: "Gudleif Skrukkefyllhaugen",
+    race: "Human",
+    origin: "Klingsdalen",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.",
+  },
+];
