500d465131f778f8189f20efa1f25bbbfa2a3ff9
ChristianSB <christiansb.kodehode@gmail.com>
2022-10-21 11:16:46 +0200

    Added background to footer

--- src/components/footer/footerStyle.js
+++ src/components/footer/footerStyle.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 
 export const FooterWrapper = styled.footer`
-  border: blue 4px double;
-  width: 100%;
   text-align: center;
+  width: 100%;
   bottom: 0;
+  border: 2px inset #5e807e;
+  padding: 1em 8em;
+  background: rgba(246, 212, 194, 0.4);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(10.5px);
+  -webkit-backdrop-filter: blur(10.5px);
 `;
